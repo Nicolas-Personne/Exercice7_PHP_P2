b6999fb99f41650ec363cb76618025d7d1f91652
root <personne.nico@gmail.com>
2018-11-20 15:28:39 +0100

    edit

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <form method="post">
-      <select name="check">
-        <option value="true">True</option>
-        <option value="false">False</option>
-      </select>
-      <input type="submit" name="button" value="Valider">
-    </form>
-    <?php
-    $isOk = $_POST['check'];
-    if (isset($_POST['button'])){
-    if($isOk == 'false'){
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <form method="post">
+    <select name="check">
+      <option value="true">True</option>
+      <option value="false">False</option>
+    </select>
+    <input type="submit" name="button" value="Valider">
+  </form>
+  <a href="http://PHP/"><button type="button" class="btn btn-danger" name="button">Accueil</button></a>
+  <?php
+  $isOk = $_POST['check'];
+  if (isset($_POST['button'])){
+    if($isOk == false){
       echo 'c\'est pas bon';
     }else{
       echo 'c\'est ok';
     }
   }
-     ?>
-  </body>
+  ?>
+</body>
 </html>
